3d5aa7096760a3aab4e22eb08b913aef02a67f9f
michalklecha <michalklecha.0@gmail.com>
2017-09-29 14:13:18 +0200

    bonus 2 - player name cached

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -1,5 +1,6 @@
 package com.excilys.formation.battleships.android.ui;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +9,35 @@ import android.widget.EditText;
 import battleships.formation.excilys.com.battleships.R;
 
 public class PlayerNameActivity extends AppCompatActivity {
+    private static final String NAME_KEY = "player_name";
+
     EditText mNameEditText;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player_name);
-        mNameEditText = (EditText) findViewById(R.id.enter_player_name);
+        preferences = getApplicationContext().getSharedPreferences("Pref", MODE_PRIVATE);
+
+        String name = preferences.getString(NAME_KEY, null);
+        if (name != null) {
+            finish(name);
+        } else {
+            setContentView(R.layout.activity_player_name);
+            mNameEditText = (EditText) findViewById(R.id.enter_player_name);
+        }
     }
 
     public void onClickButton(View view) {
         String name = mNameEditText.getText().toString();
         if (!name.isEmpty()) {
-            BattleShipsApplication.init(name);
+            preferences.edit().putString(NAME_KEY, name).apply();
+            finish(name);
         }
+    }
+
+    public void finish(String name) {
+        BattleShipsApplication.init(name);
+        super.finish();
     }
 }
